03f8042ba076e5672f3316ba7c9356e57fc762f2
Alizandro Lopez <alizanpez@gmail.com>
2018-07-19 09:55:43 -0700

    added defood

---
+++ defoodApp/Defood/Controllers/FoodViewController.swift
@@ -0,0 +1,56 @@
+//
+//  FoodViewController.swift
+//  Defood
+//
+//  Created by Alizandro Lopez on 7/18/18.
+//  Copyright © 2018 GLWR. All rights reserved.
+//
+
+
+import Foundation
+import UIKit
+import CoreLocation
+
+class FoodViewController: UIViewController, UITextFieldDelegate {
+    var location: CLLocation?
+    
+    @IBOutlet weak var pickingFoodTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(location)
+        var restaurantType = pickingFoodTextField.text
+        pickingFoodTextField.delegate = self
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchPlaceFromGoogle(place: textField.text!)
+        return true
+    }
+    
+    
+    
+    func searchPlaceFromGoogle(place: String){
+        var strGoogleApi = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=\(place)&key=AIzaSyBiw2dOmiz1teJDJ7xjxeSJykFuSzibu2g"
+        strGoogleApi = strGoogleApi.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        let urlRequest = URLRequest(url: URL(string: strGoogleApi)!)
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data,
+            response, error) in
+            if error == nil{
+                let jsonDict = try? JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                print("json == \(String(describing: jsonDict))")
+            }else{
+                
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+}
+
+
+
---
+++ defoodApp/Defood/Controllers/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Defood
+//
+//  Created by Alizandro Lopez on 7/16/18.
+//  Copyright © 2018 GLWR. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //put this is pickingLocation file
+//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6)
+//        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+//        mapView.isMyLocationEnabled = true
+//        self.view = mapView
+         
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
